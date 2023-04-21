1ab1fc4765343ef9b0cff2e1f5988068c8660670
Yar <yarkarnizov@gmail.com>
2023-04-21 20:45:05 +0300

    Some fix

--- InternetShop.cpp
+++ InternetShop.cpp
@@ -2,70 +2,67 @@
 #include <io.h>
 #include <fcntl.h>
 
-#define NC "\e[0m"
-#define RED "\e[0;31m"
-#define GRN "\e[0;32m"
-#define CYN "\e[0;36m"
-#define REDB "\e[41m"
+#define RED "\033[0;31m"
+#define GRN "\033[0;32m"
+#define WHT "\033[0;37m"
 
 using namespace std;
 
 int main()
 {
     int option;
+	cin.exceptions(cin.failbit);
+
     while (true)
     {
 		_setmode(_fileno(stdout), _O_WTEXT);
 		wcout
+			<< WHT
 			<< L"╭──────────────────────────────────╮\n"
-			<< L"│\033[0;31m              Меню                \033[0;37m│\n"
+			<< L"│" << RED << L"              Меню                " << WHT << L"│\n"
 			<< L"├──────────────────────────────────┤\n"
-			<< L"│\033[0;32m 1. Реєстрація                    \033[0;37m│\n"
-			<< L"│\033[0;32m 2. Вхід                          \033[0;37m│\n"
-			<< L"│\033[0;32m 3. Вихід                         \033[0;37m│\n"
+			<< L"│" << GRN << L" 1. Реєстрація                    " << WHT << L"│\n"
+			<< L"│" << GRN << L" 2. Вхід                          " << WHT << L"│\n"
+			<< L"│" << GRN << L" 3. Вихід                         " << WHT << L"│\n"
 			<< L"╰──────────────────────────────────╯\n"
 			<< L" Оберіть опцію(1 - 3): ";
-		/*wcout
-			<< L"╭──────────────────────────────────╮\n"
-			<< L"│" 
-			<< "\033[0;31m"
-			 << L"               Меню               "
-			<< L"\033[0;37m"					<< "│\n"
-			<< L"├──────────────────────────────────┤\n"
-			<< L"│ 1. Реєстрація                    │\n"
-			<< L"│ 2. Вхід                          │\n"
-			<< L"│ 3. Вихід                         │\n"
-			<< L"╰──────────────────────────────────╯\n"
-			<< L" Оберіть опцію(1 - 3): ";*/
-		cin >> option;
+		try
+		{
+			cin >> option;
+		}
+		catch (istream::failure e) {
+			wcout << RED << L" Введіть коректні дані!\n";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+
+		//Красиво, но не функционально, conio.h
+		/*option = _getche();
+		wcout
+			<< L"          │\n"
+			<< L"╰──────────────────────────────────╯\n";*/
 
 		switch (option)
 		{
 		case(1):
 		{
-			cout << "a";
 			break;
 		}
 		case(2):
 		{
-
 			break;
 		}
 		case(3):
 		{
-
-			break;
-		}
-		case(4):
-		{
-
+			system("pause");
+			return 0;
 			break;
 		}
 		default:
+			wcout << RED << L" Введіть коректні дані!\n";
 			break;
 		}
     }
 
-	system("pause");
     return 0;
 }
